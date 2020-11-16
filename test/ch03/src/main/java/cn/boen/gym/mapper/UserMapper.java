c2ced243c69ee9fa893fb9e360d32d8442189b33
Boen <1260414235@qq.com>
2020-11-16 13:15:53 +0800

    用户登录

--- test/ch03/src/main/java/cn/boen/gym/mapper/UserMapper.java
+++ test/ch03/src/main/java/cn/boen/gym/mapper/UserMapper.java
@@ -2,7 +2,17 @@ package cn.boen.gym.mapper;
 
 import cn.boen.gym.pojo.User;
 
+import java.util.List;
+
 public interface UserMapper {
     //登录方法
     public User loginUser(User user);
+
+
+    //查询所有
+    public List<User> userFindAll();
+
+    public int userSave();
+
+    public int userUpdate();
 }
